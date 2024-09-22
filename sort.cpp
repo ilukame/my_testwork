f106e86f13701b5ac656f308da155d9e06eeda5b
ilukame <22080768+ilukame@users.noreply.github.com>
2024-09-23 01:38:45 +0900

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Test{
+    int age;
+    int height;
+};
+
+int main(){
+    Test test[] = 
+    {
+        {3, 2},
+        {1, 3},
+        {1, 2}
+    };
+
+    int size = sizeof(test) / sizeof(Test);
+
+    sort(test, test + size, [](auto const  &lhs, auto const &rhs){
+        if(lhs.age == rhs.age) return(lhs.height < rhs.height);
+        return(lhs.age < rhs.age);
+    });
+
+    for(auto i = 0; i < size; i++) printf("%d, age = %d, height = %d\n", i, test[i].age, test[i].height);
+
+    return 0;
+}
